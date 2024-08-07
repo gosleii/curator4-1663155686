ba7d565c474ec1270e8841bf0b7ebfe088cd3781
gosleii <sleii@inbox.ru>
2024-08-07 18:58:45 +0300

    update

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ bot = telebot.TeleBot('7084337272:AAEQS4PlKLEmRw4_VMqLrD-8l4sM-o-BpIE')
 
 
 @bot.message_handler(commands=['start'])
-def main(message):
+def start_main(message):
     bot.send_message(message.chat.id, '*Nay*', parse_mode='Markdown')
 
 
 @bot.message_handler(commands=['mew'])
-def main(message):
+def mew_main(message):
     bot.send_message(message.chat.id, 'How are you?', )
 
 
 @bot.message_handler(commands=['cat'])
-def main(message):
+def cat_main(message):
     bot.send_message(message.chat.id, '*Nay, What your name?*', parse_mode='Markdown')
 
 
 @bot.message_handler(commands=['new'])
-def main(message):
+def new_main(message):
     bot.send_message(message.chat.id, 'ссылка', parse_mode='Markdown')
 
 

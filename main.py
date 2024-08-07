0d20c6073f3426d9508e8b8e516144bf73a170b7
gosleii <sleii@inbox.ru>
2024-08-07 16:06:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import telebot
+
+bot = telebot.TeleBot('7084337272:AAEQS4PlKLEmRw4_VMqLrD-8l4sM-o-BpIE')
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, '*Nay*', parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['mew'])
+def main(message):
+    bot.send_message(message.chat.id, 'How are you?', )
+
+
+@bot.message_handler(commands=['cat'])
+def main(message):
+    bot.send_message(message.chat.id, '*Nay, What your name?*', parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['new'])
+def main(message):
+    bot.send_message(message.chat.id, 'ссылка', parse_mode='Markdown')
+
+
+bot.infinity_polling()
